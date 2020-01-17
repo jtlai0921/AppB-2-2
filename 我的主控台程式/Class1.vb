1d093bd900d779a8e266e5e66f0b6ee06856a040
賴紀廷 <49492695+jtlai0921@users.noreply.github.com>
2020-01-17 16:48:38 +0800

    Initial commit

---
+++ 我的主控台程式/Class1.vb
@@ -0,0 +1,20 @@
+﻿Public Class MyTime
+    Public Hour As Integer
+    Public Minute As Integer
+    Public Second As Integer
+    ' 物件方法: 取得時間字串
+    Public Function GetTime() As String
+        Dim str As String
+        str = Hour & ":" & Minute & ":" & Second
+        Return str
+    End Function
+    ' 物件方法: 設定時間(1)
+    Public Overloads Sub SetTime(ByVal h As Integer, ByVal m As Integer, ByVal s As Integer)
+        Hour = h : Minute = m : Second = s
+    End Sub
+    ' 物件方法: 設定時間(2)
+    Public Overloads Sub SetTime(ByVal h As Integer, ByVal m As Integer)
+        Hour = h : Minute = m : Second = 0
+    End Sub
+End Class
+
---
+++ 我的主控台程式/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+       ' 宣告myTime類別的物件變數
+        Dim open, close As MyTime
+        ' 建立物件實例
+        close = New MyTime()
+        open = New MyTime()
+        ' 設定open物件的成員變數
+        open.SetTime(10, 30)
+        ' 設定close物件的成員變數
+        close.SetTime(22, 30, 0)
+        txtOutput.Text = "開張時間: " & open.GetTime() & vbNewLine
+        txtOutput.Text &= "結束時間: " & close.GetTime() & vbNewLine
+        txtOutput.SelectionStart = 0
+    End Sub
+End Class
